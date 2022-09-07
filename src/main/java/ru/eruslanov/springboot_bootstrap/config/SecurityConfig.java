a5cf43e7084f2b00aed67cce9ae3c60ff9cf3a7c
ivan-eruslanov <90084288+ivan-eruslanov@users.noreply.github.com>
2022-09-07 08:20:06 +0500

    update SecurityConfig

--- src/main/java/ru/eruslanov/springboot_bootstrap/config/SecurityConfig.java
+++ src/main/java/ru/eruslanov/springboot_bootstrap/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/main/**").hasRole("ADMIN")
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
                 .anyRequest().authenticated()
                 .and()
